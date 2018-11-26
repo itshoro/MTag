26fad73ea20ccbe26752db2bc08ce7baf0cbf417
horodev <tim.nelke@outlook.com>
2018-11-26 19:26:51 +0100

    added BPM and Genres, mirrors changes to the ID3v2Tags class

--- MTag/Tags/ITags.cs
+++ MTag/Tags/ITags.cs
@@ -10,20 +10,21 @@ namespace MusicMetaData.MetaData
         public string LeadArtist { get; protected set; }
         public string[] Artists { get; protected set; }
         public int Year { get; protected set; }
+        public int BPM { get; protected set; }
         public int Length { get; protected set; }
         public string Publisher { get; protected set; }
+        public string[] Genres { get; protected set; }
         public int TrackNumber { get; protected set; }
 
-        protected int totalSize;
 
         public abstract void ReadTags(BinaryReader reader);
         public abstract void ReadTags(Stream s);
         protected abstract byte[] ReadTag(BinaryReader reader);
 
-        public int TotalHeaderSize()
-        {
-            return totalSize;
-        }
-
+        /// <summary>
+        /// Returns The size of the area in which Tags are stored.
+        /// Excludes the 10 Bytes of ID3v2 Headers
+        /// </summary>
+        public int TagSize { get; protected set; }
     }
 }
